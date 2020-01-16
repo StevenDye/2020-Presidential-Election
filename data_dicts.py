38c2911e83f184fb4e4404e03c7422d9047095d8
Steven Dye <steven.p.dye@gmail.com>
2020-01-16 17:45:30 -0500

    Linted code

--- data_dicts.py
+++ data_dicts.py
@@ -1,58 +1,58 @@
 """This module is a repository of lists and dictionaries that will be used in the analysis"""
 
 # Education features for 2016
-education_columns_2016 = ["Percent!!Estimate!!Population 25 years and over!!Less than 9th grade",
-                          "Percent!!Estimate!!Population 25 years and over!!9th to 12th grade, no diploma",
-                          "Percent!!Estimate!!Population 25 years and over!!High school graduate (includes equivalency)",
-                          "Percent!!Estimate!!Population 25 years and over!!Some college, no degree",
-                          "Percent!!Estimate!!Population 25 years and over!!Associate's degree",
-                          "Percent!!Estimate!!Population 25 years and over!!Bachelor's degree",
-                          "Percent!!Estimate!!Population 25 years and over!!Graduate or professional degree"]
+edu_cols_2016 = ["Percent!!Estimate!!Population 25 years and over!!Less than 9th grade",
+                 "Percent!!Estimate!!Population 25 years and over!!9th to 12th grade, no diploma",
+                 "Percent!!Estimate!!Population 25 years and over!!High school graduate (includes equivalency)",
+                 "Percent!!Estimate!!Population 25 years and over!!Some college, no degree",
+                 "Percent!!Estimate!!Population 25 years and over!!Associate's degree",
+                 "Percent!!Estimate!!Population 25 years and over!!Bachelor's degree",
+                 "Percent!!Estimate!!Population 25 years and over!!Graduate or professional degree"]
 
 # Eduction featues for both years
-education_columns_2018 = ["Estimate!!Percent!!Population 25 years and over!!Less than 9th grade",
-                          "Estimate!!Percent!!Population 25 years and over!!9th to 12th grade, no diploma",
-                          "Estimate!!Percent!!Population 25 years and over!!High school graduate (includes equivalency)",
-                          "Estimate!!Percent!!Population 25 years and over!!Some college, no degree",
-                          "Estimate!!Percent!!Population 25 years and over!!Associate's degree",
-                          "Estimate!!Percent!!Population 25 years and over!!Bachelor's degree",
-                          "Estimate!!Percent!!Population 25 years and over!!Graduate or professional degree"]
+edu_cols_2018 = ["Estimate!!Percent!!Population 25 years and over!!Less than 9th grade",
+                 "Estimate!!Percent!!Population 25 years and over!!9th to 12th grade, no diploma",
+                 "Estimate!!Percent!!Population 25 years and over!!High school graduate (includes equivalency)",
+                 "Estimate!!Percent!!Population 25 years and over!!Some college, no degree",
+                 "Estimate!!Percent!!Population 25 years and over!!Associate's degree",
+                 "Estimate!!Percent!!Population 25 years and over!!Bachelor's degree",
+                 "Estimate!!Percent!!Population 25 years and over!!Graduate or professional degree"]
 
 
 # 2016 Age and Sex features
-age_sex_columns_2016 = ['Total!!Estimate!!Total population',
-                        'Total!!Estimate!!SUMMARY INDICATORS!!Median age (years)',
-                        'Total!!Estimate!!SUMMARY INDICATORS!!Sex ratio (males per 100 females)',
-                        'Male!!Estimate!!SUMMARY INDICATORS!!Median age (years)',
-                        'Female!!Estimate!!SUMMARY INDICATORS!!Median age (years)']
+age_sex_cols_2016 = ['Total!!Estimate!!Total population',
+                     'Total!!Estimate!!SUMMARY INDICATORS!!Median age (years)',
+                     'Total!!Estimate!!SUMMARY INDICATORS!!Sex ratio (males per 100 females)',
+                     'Male!!Estimate!!SUMMARY INDICATORS!!Median age (years)',
+                     'Female!!Estimate!!SUMMARY INDICATORS!!Median age (years)']
 
 # 2018 Age and Sex features
-age_sex_columns_2018 = ['Estimate!!Total!!Total population',
-                        'Estimate!!Total!!Total population!!SUMMARY INDICATORS!!Median age (years)',
-                        'Estimate!!Total!!Total population!!SUMMARY INDICATORS!!Sex ratio (males per 100 females)',
-                        'Estimate!!Male!!Total population!!SUMMARY INDICATORS!!Median age (years)',
-                        'Estimate!!Female!!Total population!!SUMMARY INDICATORS!!Median age (years)']
+age_sex_cols_2018 = ['Estimate!!Total!!Total population',
+                     'Estimate!!Total!!Total population!!SUMMARY INDICATORS!!Median age (years)',
+                     'Estimate!!Total!!Total population!!SUMMARY INDICATORS!!Sex ratio (males per 100 females)',
+                     'Estimate!!Male!!Total population!!SUMMARY INDICATORS!!Median age (years)',
+                     'Estimate!!Female!!Total population!!SUMMARY INDICATORS!!Median age (years)']
 
 
 #  2016 Race features
-race_columns_2016 = ['Percent!!RACE!!One race!!White',
-                     'Percent!!RACE!!One race!!Black or African American',
-                     'Percent!!RACE!!One race!!American Indian and Alaska Native',
-                     'Percent!!RACE!!One race!!Asian',
-                     'Percent!!HISPANIC OR LATINO AND RACE!!Total population!!Hispanic or Latino (of any race)']
+race_cols_2016 = ['Percent!!RACE!!One race!!White',
+                  'Percent!!RACE!!One race!!Black or African American',
+                  'Percent!!RACE!!One race!!American Indian and Alaska Native',
+                  'Percent!!RACE!!One race!!Asian',
+                  'Percent!!HISPANIC OR LATINO AND RACE!!Total population!!Hispanic or Latino (of any race)']
 
 # 2018 Race features
-race_columns_2018 = ['Percent Estimate!!RACE!!Total population!!One race!!White',
-                     'Percent Estimate!!RACE!!Total population!!One race!!Black or African American',
-                     'Percent Estimate!!RACE!!Total population!!One race!!American Indian and Alaska Native',
-                     'Percent Estimate!!RACE!!Total population!!One race!!Asian',
-                     'Percent Estimate!!HISPANIC OR LATINO AND RACE!!Total population!!Hispanic or Latino (of any race)']
+race_cols_2018 = ['Percent Estimate!!RACE!!Total population!!One race!!White',
+                  'Percent Estimate!!RACE!!Total population!!One race!!Black or African American',
+                  'Percent Estimate!!RACE!!Total population!!One race!!American Indian and Alaska Native',
+                  'Percent Estimate!!RACE!!Total population!!One race!!Asian',
+                  'Percent Estimate!!HISPANIC OR LATINO AND RACE!!Total population!!Hispanic or Latino (of any race)']
 
 # 2016 Income features
-income_columns_2016 = ['Households!!Estimate!!Median income (dollars)',
-                       'Households!!Estimate!!Mean income (dollars)']
+income_cols_2016 = ['Households!!Estimate!!Median income (dollars)',
+                    'Households!!Estimate!!Mean income (dollars)']
 
 
-# 2018 Income features 
-income_columns_2018 = ['Estimate!!Households!!Median income (dollars)',
-                       'Estimate!!Households!!Mean income (dollars)']
+# 2018 Income features
+income_cols_2018 = ['Estimate!!Households!!Median income (dollars)',
+                    'Estimate!!Households!!Mean income (dollars)']
--- visualizations.py
+++ visualizations.py
@@ -2,19 +2,20 @@
 
 import pandas as pd
 
+
 def display_results(actual, prediction, counties):
-    """This function returns the number of votes each county received and the number
-    of votes they were projected to get"""
-    
-    final_results = pd.DataFrame(prediction).set_index(actual.index)
-    final_results = pd.merge(actual, final_results, left_index=True, right_index=True)
-    final_results = pd.merge(counties, final_results, left_index=True, right_index=True)
-    final_results = final_results.rename(columns={0: "Predictions"})
-    final_results['Percent off'] = final_results.apply(lambda row: (row.iloc[2]-row.iloc[1])/row.iloc[1]*100, axis=1)
-    final_results['Percent off Abs'] = final_results['Percent off'].abs() 
-    
-    print(final_results['Donald Trump'].sum(), final_results['Predictions'].sum(), final_results['Percent off Abs'].sum()/len(actual))
-    
-    return final_results
+    """This function returns the number of votes each county received and the
+    number of votes they were projected to get"""
 
+    res = pd.DataFrame(prediction).set_index(actual.index)
+    res = pd.merge(actual, res, left_index=True, right_index=True)
+    res = pd.merge(counties, res, left_index=True, right_index=True)
+    res = res.rename(columns={0: "Predictions"})
+    res['Percent off'] = res.apply(lambda row: (row.iloc[2]-row.iloc[1])/row.iloc[1]*100, axis=1)
+    res['Percent off Abs'] = res['Percent off'].abs()
 
+    print(res['Donald Trump'].sum(),
+          res['Predictions'].sum(),
+          res['Percent off Abs'].sum()/len(actual))
+
+    return res
