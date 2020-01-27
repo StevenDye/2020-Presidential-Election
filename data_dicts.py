736e9ac9e9716f6320fe5750c479e01bba89d2a4
Steven Dye <steven.p.dye@gmail.com>
2020-01-27 11:55:22 -0500

    Included states_list

--- data_dicts.py
+++ data_dicts.py
@@ -126,6 +126,15 @@ income_2018 = ['Estimate!!Households!!Median income (dollars)',
 drop_cols = ['County', 'State', 'Republican', 'Democrat', 'Other', 'Total population',
              'Percent Republican', 'Percent Democrat', 'Percent Other']
 
+states_list = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado',
+               'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia',
+               'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky',
+               'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+               'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+               'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+               'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 
+               'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+               'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 
 Electoral_dict = {'Alabama': 9,
                   'Alaska': 3,
